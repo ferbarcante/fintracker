859ce627de7c5a2feb926f19559637364b401dc8
ferbarcante <barcantef@gmail.com>
2025-01-02 19:21:08 -0300

    adicionando conexão com postgres

---
+++ template/app/config/fintracker.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'  =>  "localhost",
+    'port'  =>  "5432",
+    'name'  =>  "fintracker",
+    'user'  =>  "postgres",
+    'pass'  =>  "123",
+    'type'  =>  "pgsql",
+    'prep'  =>  "1"
+];
