f67fb9d2007e2c5632e80b2f235cadbc2a312aaa
ferbarcante <barcantef@gmail.com>
2025-01-29 09:51:25 -0300

    feat: model de conta bancária

--- app/model/fintracker/Conta.php
+++ app/model/fintracker/Conta.php
@@ -4,9 +4,11 @@ use Adianti\Database\TRecord;
 
 class Conta extends TRecord
 {
-    const TABLENAME = 'conta';
+    const TABLENAME = 'conta_bancaria';
     const PRIMARYKEY ='id_conta';
     const IDPOLICY = 'max';
+
+    private $tipoConta;
 
     public function __construct($id = NULL, $callObjectLoad = TRUE)
     {
@@ -14,8 +16,15 @@ class Conta extends TRecord
         parent::addAttribute('id_conta');
         parent::addAttribute('nm_conta');
         parent::addAttribute('vl_saldo');
-        parent::addAttribute('tp_conta');
+        parent::addAttribute('id_tipoconta');
     }
 
-
+    function get_tipo_conta()
+    {
+        if(empty($this->tipoConta))
+        $this->tipoConta = new TipoConta($this->id_tipoConta);
+    
+        // returns the associated object
+        return $this->tipoConta;
+    }
 }
